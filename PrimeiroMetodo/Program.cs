4591e09040835dceda5f8b57b4d8a7ebbe40363e
EdmarSantosM <santosem4@hotmail.com>
2023-04-30 22:09:09 -0300

    Utilizando Random
    gerando  número aleatórios

--- PrimeiroMetodo/Program.cs
+++ PrimeiroMetodo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static void Main(string[]arg)
+    {
+            //chamar  o método
+            gerarNumeros();
+
+
+        //criação do metodo
+        void gerarNumeros()
+        {
+            Random gerar = new Random();
+            
+            System.Console.WriteLine("\nGeração de números aleatórios:\n");
+
+            //loop de iteração
+            for(int i = 0; i < 5; i++)
+            {
+                System.Console.WriteLine($"\nNumeros Gerados = {gerar.Next(1,100)}");
+            }
+
+        }
+        System.Console.WriteLine();
+        System.Console.WriteLine($"\nPressione para 'ENTER' para sair do programa");
+        Console.ReadKey();
+    }
+}
